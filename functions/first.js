dd7ec16444cdb0d285de61b020d6d424b5282378
gautam-chitti <119092331+gautam-chitti@users.noreply.github.com>
2025-02-06 16:46:36 +0000

    function Menu

---
+++ functions/first.js
@@ -0,0 +1,10 @@
+function SayMyName(){
+    console.log("You are gautam")
+}
+// SayMyName  /// this is know as the function reference that the function is there
+
+function addtwoNum(num1, num2){
+    return num1 + num2
+}
+
+console.log(addtwoNum(5,7))
