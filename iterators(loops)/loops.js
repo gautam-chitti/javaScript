5e4ebea4d6a2b4d0f7f2de26cdd5416260789951
gautam-chitti <119092331+gautam-chitti@users.noreply.github.com>
2025-02-08 06:25:30 +0000

    loops

---
+++ iterators(loops)/loops.js
@@ -0,0 +1,26 @@
+for(let index = 0; index < 10; index++){
+    const element = index;
+    // console.log(element);
+    
+}
+
+let myarr = [1,2,3,4,5,6,7]
+for (let index = 0; index < myarr.length; index++) {
+    const element = myarr[index];
+    
+}
+ 
+// while loop
+let i = 1
+while (i<=10){
+    // console.log(i)
+    i++
+}
+
+
+// do while it will run atleast once, it will " do " then check the condition
+do{
+    console.log(i)
+    i = i + 1
+}while(i<1) // i will be 11 since it was incremented to be 10 in the previous while loop
+
