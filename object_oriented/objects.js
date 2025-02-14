84eccdde5bbe0088c6b079d3c2e5fe7c6fb342ae
gautam-chitti <119092331+gautam-chitti@users.noreply.github.com>
2025-02-14 16:01:44 +0000

    objects

---
+++ object_oriented/objects.js
@@ -0,0 +1,5 @@
+// oop is programming paradigm , 
+// javascript is prototype based 
+// classes were added in es6 which are not really anything but just sugar coating 
+// this was just for that the js code becomes more organised and resuseable
+
